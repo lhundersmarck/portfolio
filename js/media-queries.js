b31caa2d9b6ff022a906494eb6b8e7f57068d6fa
lhundersmarck <laurahundersmarck@gmail.com>
2019-06-19 21:53:58 -0400

    updated nav bar, background image on about page and buttons, buttons not working on firefox

--- js/media-queries.js
+++ js/media-queries.js
@@ -1,18 +1,29 @@
-const minWidth575 = window.matchMedia("(min-width: 575px)");
-handleMinWidth575(minWidth575);
+// Match media query of min-width 991px
+const minWidth991 = window.matchMedia("(min-width: 991px)");
+// Add a listener to know when the width changes
+minWidth991.addListener(handleMinWidth991);
 
-function handleMinWidth575(minWidth575) {
-  if (minWidth575.matches) {
+// When the page loads for the first time, we're not going to be notified of the width change
+// cause there wasn't any. To apply the correct rule, we need to make sure to call the function
+// right after the page loads
+document.addEventListener("DOMContentLoaded", function(){
+    handleMinWidth991(minWidth991)
+});
+
+function handleMinWidth991(minWidth991) {
+  // If the media query is matched (screen width is at least 991px),
+  // put the content div with text inside the div that has the background image
+  // so that it jumps up
+  if (minWidth991.matches) {
     const aboutHeroContentDiv = document.getElementsByClassName("about-hero-content")[0];
     const aboutHeroImageDiv = document.getElementsByClassName("about-hero-image")[0];
-    if (aboutHeroImageDiv) {
-      aboutHeroImageDiv.appendChild(aboutHeroContentDiv);
-    }
+    aboutHeroImageDiv.appendChild(aboutHeroContentDiv);
   } else {
+    // If the media query doesn't match (screen width is less than 991px),
+    // take the content div with text and put it back inside the sections
+    // so that it doesn't overlap the image
     const aboutHeroContentDiv = document.getElementsByClassName("about-hero-content")[0];
     const aboutSection = document.getElementById("about-hero");
     aboutSection.appendChild(aboutHeroContentDiv);
   }
 }
-
-minWidth575.addListener(handleMinWidth575);
